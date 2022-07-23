0bbf4b0c1cda1e5081a53903005dac1f0e0f4572
Mark Linn <marktlinn@gmail.com>
2022-07-23 21:56:29 +0200

    started simple project from Scrimba, finished navbar layout using Figma

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 import Main from './components/Main'
 import Nav from './components/Nav'
 function App() {
--- react_project/src/components/Nav.jsx
+++ react_project/src/components/Nav.jsx
@@ -1,9 +1,11 @@
 import react from 'react'
-
+import reactLogo from "../assets/reactLogo.svg"
 export default function Nav () {
 return  (
-    <nav>
-        <h1>This is a Nav</h1>
+    <nav className='nav-bar'>
+        <img src={reactLogo} alt="React logo" />
+        <h2 className='nav-title'>ReactFacts</h2>
+        <h3 className='project-num'>React Course - Project 1</h3>
     </nav>
 )
 }
