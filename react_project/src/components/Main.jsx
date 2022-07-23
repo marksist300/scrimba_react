2a49f24363af3ef69a9253615d2cc651c7e298d4
Mark Linn <marktlinn@gmail.com>
2022-07-23 21:08:55 +0200

    Created components and rendered them to the DOM

---
+++ react_project/src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Main () {
+    return (
+        <main> 
+            <h1>This is the Main</h1>
+        </main>
+    )
+}
+
---
+++ react_project/src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import react from 'react'
+
+export default function Nav () {
+return  (
+    <nav>
+        <h1>This is a Nav</h1>
+    </nav>
+)
+}
