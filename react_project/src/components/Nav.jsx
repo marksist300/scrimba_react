96ae4674bf71b76b9f0425c19f9bfedc354781eb
Mark Linn <marktlinn@gmail.com>
2022-07-24 11:21:59 +0200

    fixed line height in nav and made responsive

--- react_project/src/components/Nav.jsx
+++ react_project/src/components/Nav.jsx
@@ -3,7 +3,7 @@ import reactLogo from "../assets/reactLogo.svg"
 export default function Nav () {
 return  (
     <nav className='nav-bar'>
-        <img src={reactLogo} alt="React logo" />
+        <img className='react-logo' src={reactLogo} alt="React logo" />
         <h2 className='nav-title'>ReactFacts</h2>
         <h3 className='project-num'>React Course - Project 1</h3>
     </nav>
