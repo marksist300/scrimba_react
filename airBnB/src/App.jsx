abe7d8269e99c495f2269b931fcdb2373874cfc8
Mark Linn <marktlinn@gmail.com>
2022-07-27 10:24:03 +0200

    updated CSS to complete styling of card elements, all elements other than the status cards on image are passed as props

--- airBnB/src/App.jsx
+++ airBnB/src/App.jsx
@@ -3,7 +3,22 @@ import Nav from "./components/Nav"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
 import data from "./data"
- console.log(data)
+const cardOutputData = data.map(elem=> {
+  return (
+    <Card 
+        id={elem.id}
+        title={elem.title}
+        description={elem.description}
+        price={elem.price}
+        img={elem.coverImg}
+        rating={elem.stats.rating}
+        reviewCount={elem.stats.reviewCount}
+        location={elem.location}
+        openSpots={elem.openSpots}
+    />
+  )
+})
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,14 +26,9 @@ function App() {
     <div className='container'>
       <Nav />
       <Hero />
-      <Card 
-        img="./src/assets/swimmer.png"
-        rating={5}
-        reviewCount={6}
-        location="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      <div className='card-section'>
+      {cardOutputData}
+      </div>
     </div>
   )
 }
